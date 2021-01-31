1da2c01eb2895ea41426a43ab39d670b2cd4db85
Ori Markowitz <ori.markowitz@gmail.com>
2021-01-31 16:26:07 -0500

    commit tree 8b69c832dd3f20cf2a1f4e2d145aeb3eaaae60fb

--- src/components/App.js
+++ src/components/App.js
@@ -16,12 +16,20 @@ function Home() {
   );
 }
 
+const About = () => {
+  return (
+    <div id="about">
+      About
+    </div>
+  )
+}
+
 function App() {
   return (
     <div>
       <NavBar />
-      {/* add the <Home> component here */}
-      {/* write an <About> component here */}
+      <Home />
+      <About />
     </div>
   );
 }
